72fe22740d60be36204b9bc9ac8b215d0f37afd0
RianAina <hei.rianaina@gmail.com>
2024-10-03 09:53:55 +0300

    feat: commit all

---
+++ web2-rattrapage-app/src/App.js
@@ -0,0 +1,41 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const url = "http://localhost:4000/apartments";
+  const [data, setData] = useState([]);
+
+  const fetchInfo = () => {
+    return axios.get(url).then((res) => setData(res.data));
+  };
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1 style={{ color: "black" }}>All appartements</h1>
+      <center>
+        {data.map((dataObj, index) => {
+          return (
+            <div key={index}>  
+
+        
+              <img src={dataObj.picture} alt={dataObj.description}></img>
+              <p style={{ fontSize: 20, color: 'black' }}>{dataObj.description}</p> 
+              <p style={{ fontSize: 20, color: 'grey' }}>Prix: {dataObj.price} €</p>
+              
+
+            </div>
+
+            
+          );
+        })}
+      </center>
+    </div>
+  );
+}
+
+export default App;
